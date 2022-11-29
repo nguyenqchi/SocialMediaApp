f751be41f4636223bd0f8eedf3bc3005e647690e
Chloe Nguyen <cnguyen2@conncoll.edu>
2022-11-29 15:06:25 -0500

    add follower method

--- User.java
+++ User.java
@@ -17,19 +17,47 @@ public class User{
 		allPost.insert(newpost);
 	}
 	public void displayAllPost(){
-		allPost.inOrder(allPost.root);
+		if(allPost.root == null){
+			System.out.println("You haven't posted anything recently.");
+		}
+		else{
+			allPost.inOrder(allPost.root);
+		}
 	}
 	public void displayFollowers(){
-		followers.display();
+		if(followers.isEmpty()){
+			System.out.println("You don't have any followers.");
+		}
+		else{
+			System.out.println("Your list of followers is: ");
+			followers.display();
+		}
 	}
 	public void displayFollowing(){
-		following.display();
+		if(following.isEmpty()){
+			System.out.println("You are not following anyone");
+		}
+		else{
+			System.out.println("You are following: ");
+			following.display();
+		}
+		
+	}
+	public void addFollower(String s){
+		followers.addFirst(s);
+	}
+	public void addFollowing(String s){
+		following.addFirst(s);
 	}
 	public static void main(String[] args){
 		User me = new User("cnguyen2");
 		me.addPost("Hello World");
 		me.addPost("Merry Christmas");
 		me.displayAllPost();
+		me.addFollower("nle6");
+		me.addFollowing("anahi");
+		me.displayFollowers();
+		me.displayFollowing();
 	}
 
 
