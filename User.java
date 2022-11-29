0913cf2ec92dcccd1c5bbb8ed629f52add30a8a6
Chloe Nguyen <cnguyen2@conncoll.edu>
2022-11-28 20:17:40 -0500

    add User class

---
+++ User.java
@@ -0,0 +1,31 @@
+public class User{
+	
+	String displayName;
+	String loginName;
+	String password;
+	AVLTree allPost = new AVLTree();
+	LinkedList followers = new LinkedList();
+	LinkedList following = new LinkedList();
+
+	
+	public User(String name){
+		loginName = name;
+	}
+
+	public void addPost(String s){
+		Post newpost = new Post(s, loginName);
+		allPost.insert(newpost);
+	}
+	public void displayAllPost(){
+		allPost.inOrder(allPost.root);
+	}
+
+	public static void main(String[] args){
+		User me = new User("cnguyen2");
+		me.addPost("Hello World");
+		me.addPost("Merry Christmas");
+		me.displayAllPost();
+	}
+
+
+}
