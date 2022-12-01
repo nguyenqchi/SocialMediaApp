ef231dbefdee9266a822abaac208b8366a2c299a
AnahiLop <115180509+AnahiLop@users.noreply.github.com>
2022-11-30 20:41:31 -0500

    hashmap

--- HashMap.java
+++ HashMap.java
@@ -1,3 +1,44 @@
-public class HashMap{
+import java.util.ArrayList;
+
+public class HashNode{
+
+	String key;
+	User value;
+	HashNode next;
+	public HashNode(String loginname, User user>){
+		this.key = element;
+		this.value = value;
+	}
+
+}
+
+
+public class HashMap<K,V> {
+	private HashNode[] bucketArray;
+	private int numBuckets;
+	private int size;
+
+	public HashMap(int cap){
+		bucketArray = new HashNode[cap];
+		numBuckets = 911;
+		size = 0;
+
+		for(int i= 0; i < numBuckets; i++){
+			bucketArray.add(null);
+		}
+
+	}
+
+	public int size(){
+		return size; 
+	}
+
+	public boolean isEmpty(){
+		return size() == 0;
+	}
+
+	
+
+
 	
 }
